e8c5e1765508ebbf8387086215f6596b9fa380dc
Val Milkevich <milkevich@gmail.com>
2011-09-06 22:37:56 +0300

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hz::Application.config.secret_token = '77ae9c323a7f5dee1d9f50737c9740c13393c837703809d4e2781a24f6bff32b551a069ae36b5731994ebb92f96b6b995baa638bd3819c0b5e494a58036553fa'
